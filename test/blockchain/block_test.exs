10b7af5260da93ff7d93297761f69c20ee79b1c8
Leonardo Avanzi <47260167+leonardoavanzi@users.noreply.github.com>
2021-09-01 14:39:11 -0300

    undescore hash on assert test

--- test/blockchain/block_test.exs
+++ test/blockchain/block_test.exs
@@ -10,7 +10,7 @@ defmodule RousChain.BlockChain.BlockTest do
     test "genesis alive" do
       assert %Block{
                data: "genesis-data",
-               hash: "genesis-hash",# the genesis block doesn't have a last hash
+               hash: "F277BF9150CD035D55BA5B48CB5BCBE8E564B134E5AD0D56E439DD04A1528D3B",# the genesis block doesn't have a last hash
                last_hash: "-",
                timestamp: "1_599_909_623_805_627" # random timestamp
              } == Block.genesis()
@@ -31,8 +31,8 @@ defmodule RousChain.BlockChain.BlockTest do
       data = "new block data"
       last_hash = "random_hash_last_block"
 
-      assert %Block{timestamp: ^timestamp, hash: hash, last_hash: ^last_hash, data: ^data} =
-               Block.new(timestamp, hash, last_hash, data)
+      assert %Block{timestamp: ^timestamp, hash: _hash, last_hash: ^last_hash, data: ^data} =
+               Block.new(timestamp,last_hash, data)
     end
   end
 end
