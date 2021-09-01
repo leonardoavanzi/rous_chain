e29bfc115e682867d8c020b1c521ba52906246c3
Leonardo Avanzi <47260167+leonardoavanzi@users.noreply.github.com>
2021-09-01 14:57:53 -0300

    fix 2

--- lib/blockchain/block.ex
+++ lib/blockchain/block.ex
@@ -16,10 +16,39 @@ defmodule RousChain.BlockChain.Block do
   defstruct ~w(timestamp last_hash hash data)a # ~w ("timestamp", "last_hash", "hash", "data")
 
 
-  @spec new(pos_integer(), String.t(), any()) :: Block.t()
-  def new(timestamp, hash, last_hash, data) do
-    %__MODULE__{timestamp: timestamp, last_hash: last_hash, hash: hash, data: data}
+  # defp func
+  defp add_timestamp(%__MODULE__{} = block, timestamp), do: %{block | timestamp: timestamp}
+
+
+  defp add_data(%__MODULE__{} = block, data), do: %{block | data: data}
+
+  defp add_last_hash(%__MODULE__{} = block, last_hash), do: %{block | last_hash: last_hash}
+
+  defp add_hash(%__MODULE__{timestamp: timestamp, last_hash: last_hash, data: data} = block) do
+    %{block | hash: hash(timestamp, last_hash, data)}
   end
+
+
+  @spec new(pos_integer(), String.t(), any()) :: Block.t()
+  def new(timestamp, last_hash, data) do
+    %__MODULE__{}
+    |> add_timestamp(timestamp)
+    |> add_last_hash(last_hash)
+    |> add_data(data)
+    |> add_hash()
+  end
+
+  @spec get_str(Block.t()) :: String.t()
+  def get_str(block = %__MODULE__{}) do
+    """
+    Block
+    timestamp: #{block.timestamp}
+    last_hash: #{block.last_hash}
+    hash: #{block.last_hash}
+    data: #{block.data}
+    """
+  end
+
 
   @spec genesis() :: Block.t()
   def genesis() do
@@ -38,6 +67,5 @@ defmodule RousChain.BlockChain.Block do
     data = "#{timestamp}:#{last_hash}:#{Jason.encode!(data)}"
     Base.encode16(:crypto.hash(:sha256, data))
   end
-
 
 end
